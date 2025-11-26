a7de1be6de989186fe50554d0853762cd58c6b28
Bailly Hugo <gucci60610@gmail.com>
2025-11-26 16:06:26 +0100

    functoonnal restaurant review webappA

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+Restaurant.create!(name: "Epicure", address: "75008 Paris", phone_number: "0101010101", category: "french")
+Restaurant.create!(name: "Sushi Zen", address: "Tokyo", phone_number: "0202020202", category: "japanese")
+Restaurant.create!(name: "Pasta Forever", address: "Roma", phone_number: "0303030303", category: "italian")
+Restaurant.create!(name: "Shanghai Express", address: "Beijing", phone_number: "0404040404", category: "chinese")
+Restaurant.create!(name: "Belgian Waffles", address: "Bruxelles", phone_number: "0505050505", category: "belgian")
